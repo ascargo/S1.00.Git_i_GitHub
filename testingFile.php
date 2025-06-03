31e69de801a9d2bda1c5c43a128c8b24aab4b317
Asier Carrasco González <axiercarrasco@gmail.com>
2025-06-03 11:01:32 +0200

    Correction content testingFile.php

--- testingFile.php
+++ testingFile.php
@@ -1,3 +1,4 @@
 <?php
-echo "This is a testing file for the IT Academy exercise 0, where we are looking at working with Git and GitHub.<br>"; "
+echo "This is a testing file for the IT Academy's Sprint 1 Exercise 0,
+where we are looking at working with Git and GitHub.<br>"; "
 ?>
